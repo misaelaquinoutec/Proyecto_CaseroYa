563fb62a54ca3c05ee13a26a25cb62df115e5f4c
misaelaquinoutec <misael.aquino@utec.edu.pe>
2026-09-15 15:19:13 -0500

    Primer commit

---
+++ src/main/java/com/proyect/caseroya/CaseroyaApplication.java
@@ -0,0 +1,13 @@
+package com.proyect.caseroya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseroyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseroyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyect/caseroya/CaseroyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.caseroya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseroyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
